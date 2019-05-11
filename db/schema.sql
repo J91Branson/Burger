b9cd2c8d466bd8f85947d6a3d809d2b1062cf99e
J91Branson <jbcod4@gmail.com>
2019-05-10 22:16:38 -0500

    complete app with css

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,16 @@
-### Schema
+drop database if exists burgers_db;
+drop database if exists burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
-CREATE DATABASE cat_db;
-USE cat_db;
-
-CREATE TABLE cats
+CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	eaten BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+
+
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheeseburger', true);
